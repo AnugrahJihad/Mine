d24a72d8726400ccb9f19ab318deb581528e1490
unknown <anugrahjihadn@gmail.com>
2023-12-14 22:03:36 +0700

    Update

---
+++ Spotipay/index.js
@@ -0,0 +1,101 @@
+const image = document.getElementById('cover'),
+    title = document.getElementById('music-title'),
+    artist = document.getElementById('music-artist'),
+    currentTimeEl = document.getElementById('current-time'),
+    durationEl = document.getElementById('duration'),
+    progress = document.getElementById('progress'),
+    playerProgress = document.getElementById('player-progress'),
+    prevBtn = document.getElementById('prev'),
+    nextBtn = document.getElementById('next'),
+    playBtn = document.getElementById('play'),
+    background = document.getElementById('bg-img');
+
+const music = new Audio();
+
+const songs = [
+    {
+        path: 'Aset/Weird Genius - Lathi (ft. Sara Fajira) Official Music Video(MP3_160K).mp3',
+        displayName: 'Lathi',
+        cover: 'Aset/pexels-aron-visuals-1643130.jpg',
+        artist: 'Weird Genius (ft. Sara Fajira)',
+    },
+    {
+        path: 'Aset/stay with me - 1nonly (sodaboi remix)(MP3_160K).mp3',
+        displayName: 'Stay With Me',
+        cover: 'Aset/pexels-ryutaro-tsukata-5220092.jpg',
+        artist: '1nonly',
+    },
+    {
+        path: 'Aset/Wellerman - Sea Shanty [TikTok Remix] [Bass Boosted] ft. 220 KID _ Billen Ted(MP3_160K).mp3',
+        displayName: 'WEllerman',
+        cover: 'Aset/pexels-anthony-🙂-5626573.jpg',
+        artist: 'Sea Shanty',
+    }
+];
+
+let musicIndex = 0;
+let isPlaying = false;
+
+function togglePlay() {
+    if (isPlaying) {
+        pauseMusic();
+    } else {
+        playMusic();
+    }
+}
+
+function playMusic() {
+    isPlaying = true;
+
+    playBtn.classList.replace('fa-play', 'fa-pause');
+
+    playBtn.setAttribute('title', 'Pause');
+    music.play();
+}
+
+function pauseMusic() {
+    isPlaying = false;
+
+    playBtn.classList.replace('fa-pause', 'fa-play');
+    playBtn.setAttribute('title', 'Play');
+    music.pause();
+}
+
+function loadMusic(song) {
+    music.src = song.path;
+    title.textContent = song.displayName;
+    artist.textContent = song.artist;
+    image.src = song.cover;
+    background.src = song.cover;
+}
+
+function changeMusic(direction) {
+    musicIndex = (musicIndex + direction + songs.length) % songs.length;
+    loadMusic(songs[musicIndex]);
+    playMusic();
+}
+
+function updateProgressBar() {
+    const { duration, currentTime} = music;
+    const progressPercent = (currentTime / duration) * 100;
+    progress.style.width = `${progressPercent}%`;
+
+    const formatTime = (time) => String(Math.floor(time)).padStart(2, '0');
+    durationEl.textContent = `${formatTime(duration / 60)}:${formatTime(duration % 60)}`;
+    currentTimeEl.textContent = `${formatTime(currentTime / 60)}:${formatTime(currentTime % 60)}`;
+}
+
+function setProgressBar(e) {
+    const width = playerProgress.clientWidth;
+    const clickX = e.offsetX;
+    music.currentTime = (clickX / width) * music.duration;
+}
+
+playBtn.addEventListener('click', togglePlay);
+prevBtn.addEventListener('click', () => changeMusic(-1));
+nextBtn.addEventListener('click', () => changeMusic(1));
+music.addEventListener('ended', () => changeMusic(1));
+music.addEventListener('timeupdate', updateProgressBar);
+playerProgress.addEventListener('click', setProgressBar);
+
+loadMusic(songs[musicIndex]);
